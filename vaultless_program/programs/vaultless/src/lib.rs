918c0096c2285e1435142050b34f61890c3c46cf
chhhee10 <chetanraghuvanshi85@gmail.com>
2026-05-08 23:58:44 +0530

    Configured Solana settings and phantom wallet, Fixed UI changes and add EmailJS services

--- vaultless_program/programs/vaultless/src/lib.rs
+++ vaultless_program/programs/vaultless/src/lib.rs
@@ -11,7 +11,7 @@ pub mod vaultless {
     /// In Vaultless V2, we store the Fuzzy Extractor's "Helper Data" on-chain.
     pub fn initialize_identity(ctx: Context<InitializeIdentity>, helper_data: String) -> Result<()> {
         let identity = &mut ctx.accounts.identity;
-        identity.owner = ctx.accounts.user.key();
+        identity.owner = ctx.accounts.owner.key();
         identity.helper_data = helper_data;
         identity.enrolled_at = Clock::get()?.unix_timestamp;
         identity.last_auth_at = 0;
@@ -64,7 +64,7 @@ pub mod vaultless {
 pub struct InitializeIdentity<'info> {
     #[account(
         init, 
-        payer = user, 
+        payer = owner, 
         // Space breakdown:
         // 8  bytes  — Anchor account discriminator
         // 32 bytes  — owner Pubkey
@@ -74,13 +74,13 @@ pub struct InitializeIdentity<'info> {
         // 8  bytes  — last_auth_at i64 timestamp
         // 1  byte   — is_locked bool
         space = 8 + 32 + (4 + 4096) + 8 + 8 + 1, 
-        seeds = [b"identity", user.key().as_ref()],
+        seeds = [b"identity", owner.key().as_ref()],
         bump
     )]
     pub identity: Account<'info, Identity>,
     
     #[account(mut)]
-    pub user: Signer<'info>,
+    pub owner: Signer<'info>,
     
     pub system_program: Program<'info, System>,
 }
@@ -89,36 +89,36 @@ pub struct InitializeIdentity<'info> {
 pub struct UpdateIdentity<'info> {
     #[account(
         mut,
-        seeds = [b"identity", user.key().as_ref()],
+        seeds = [b"identity", owner.key().as_ref()],
         bump,
         has_one = owner
     )]
     pub identity: Account<'info, Identity>,
-    pub user: Signer<'info>,
+    pub owner: Signer<'info>,
 }
 
 #[derive(Accounts)]
 pub struct TriggerDuress<'info> {
     #[account(
         mut,
-        seeds = [b"identity", user.key().as_ref()],
+        seeds = [b"identity", owner.key().as_ref()],
         bump,
         has_one = owner
     )]
     pub identity: Account<'info, Identity>,
-    pub user: Signer<'info>,
+    pub owner: Signer<'info>,
 }
 
 #[derive(Accounts)]
 pub struct Authenticate<'info> {
     #[account(
         mut,
-        seeds = [b"identity", user.key().as_ref()],
+        seeds = [b"identity", owner.key().as_ref()],
         bump,
         has_one = owner
     )]
     pub identity: Account<'info, Identity>,
-    pub user: Signer<'info>,
+    pub owner: Signer<'info>,
 }
 
 #[account]
